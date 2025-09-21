0ac42c4806fe41ad73798785f8768e6c2931c77a
marwancoder <marwane.ouarradi@gmail.com>
2025-09-22 00:55:36 +0100

    Seed the root admin

--- job-backoffice/database/seeders/DatabaseSeeder.php
+++ job-backoffice/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // seed the root admin
+        User::create([
+            "name" => "Admin",
+            "email" => "admin@admin.com",
+            "password" => Hash::make("123456789"),
+            "role" => "admin",
+            "email_verified_at" => now()
         ]);
     }
 }
