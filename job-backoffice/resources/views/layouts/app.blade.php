4d357dd45005a18f25fecd7e8148eca30f105630
marwancoder <marwane.ouarradi@gmail.com>
2025-09-23 14:56:58 +0100

    Fix Login Page + Links in SideBar + Make Controllers

--- job-backoffice/resources/views/layouts/app.blade.php
+++ job-backoffice/resources/views/layouts/app.blade.php
@@ -15,22 +15,24 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="flex">
             @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            
+            <div class="flex-1 min-h-screen bg-gray-100">
+                <!-- Page Heading -->
+                @isset($header)
+                    <header class="bg-white shadow">
+                        <div class="w-full px-5 py-5">
+                            {{ $header }}
+                        </div>
+                    </header>
+                @endisset
+    
+                <!-- Page Content -->
+                <main>
+                    {{ $slot }}
+                </main>
+            </div>
         </div>
     </body>
 </html>
