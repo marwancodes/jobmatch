1505edf24a96d360400bcdce34e8a15c547d2482
marwancoder <marwane.ouarradi@gmail.com>
2025-11-19 14:52:25 +0000

    Fix dropdown's background color

--- job-app/resources/views/components/dropdown-link.blade.php
+++ job-app/resources/views/components/dropdown-link.blade.php
@@ -1,1 +1,4 @@
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 bg-black/90 text-white hover:bg-black/80 focus:outline-none focus:bg-black/80 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 bg-black/90 text-white hover:bg-[#18181b] 
+    focus:outline-none focus:bg-black/80 transition duration-150 ease-in-out']) }}>
+    {{ $slot }}
+</a>
